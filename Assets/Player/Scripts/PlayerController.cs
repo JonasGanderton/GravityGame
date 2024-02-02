90d987ff682b4d14b804baded9132bcbbf25b223
JonasGanderton <jonasganderton@gmail.com>
2024-02-02 22:41:08 +0000

    Add player ship with flight controls

---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+	private Rigidbody2D _rigidbody2D;
+
+	private void Awake()
+	{
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	public void Accelerate(float acceleration)
+	{
+		_rigidbody2D.AddRelativeForce(new Vector2(0f, acceleration));
+	}
+
+	public void Rotate(float rotation)
+	{
+		// Apply smoothing to rotation?
+		_rigidbody2D.transform.Rotate(0, 0, rotation);
+	}
+}
