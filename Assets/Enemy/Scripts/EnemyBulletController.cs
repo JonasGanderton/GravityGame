8436408302cb564a529d8a4999be20b9ea9bd0a9
JonasGanderton <jonasganderton@gmail.com>
2024-04-06 17:50:14 +0200

    Enemy shoots at player

---
+++ Assets/Enemy/Scripts/EnemyBulletController.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    [SerializeField] private float firingDelay = 1f;
+    
+    [SerializeField] private GameObject bulletPrefab;
+    private GameObject[] _bulletPrefabs;
+    
+    private BulletBehaviour[] _bullets;
+    private const int MaxBullets = 2;
+    private int _nextBulletIndex = 0;
+    
+    private float _nextFireTime;
+
+    private void Awake()
+    {
+        _bullets = new BulletBehaviour[MaxBullets];
+        _bulletPrefabs = new GameObject[MaxBullets];
+
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            _bulletPrefabs[i] = Instantiate(bulletPrefab);
+            _bullets[i] = _bulletPrefabs[i].GetComponent<BulletBehaviour>();
+        }
+        
+        _nextFireTime = Time.time + firingDelay;
+
+    }
+
+    private void SetNextBulletIndex()
+    {
+        _nextBulletIndex++;
+        if (_nextBulletIndex == MaxBullets)
+        {
+            _nextBulletIndex = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Time.time < _nextFireTime) return; // Check if there has been a long enough gap
+        if (_bullets[_nextBulletIndex].IsActive()) return; // Check if there are any available bullets
+        _nextFireTime = Time.time + firingDelay;
+        _bullets[_nextBulletIndex].Fire(transform.position, transform.rotation);
+        SetNextBulletIndex();
+    }
+}
