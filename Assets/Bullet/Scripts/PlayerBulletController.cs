c996ee29839128fddf1b1443afd0c4118803d39d
JonasGanderton <jonasganderton@gmail.com>
2024-05-10 08:37:41 +0100

    Increase player bullets to 50

--- Assets/Bullet/Scripts/PlayerBulletController.cs
+++ Assets/Bullet/Scripts/PlayerBulletController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class PlayerBulletController : BulletController
-{ 
+{
+    private new void Awake()
+    {
+        MaxBullets = 50;
+        base.Awake();
+    }
     public void TryShootingWeapon()
     {
         Shoot();
